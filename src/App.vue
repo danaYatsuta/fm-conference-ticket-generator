48c07b613f8aad63fc94da2faf9429a2cd9f471c
Dana Yatsuta <danayatsuta@gmail.com>
2025-10-22 21:16:29 +0300

    Mobile text section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="background-mobile flex h-dvh flex-col items-center pt-8">
-    <header>
+  <div class="background-mobile text-neutral-0 font-inconsolata h-dvh px-4 pt-8">
+    <header class="flex justify-center">
       <img src="./assets/images/logo-full.svg" alt="Coding Conf logo" class="w-40" />
     </header>
+
+    <main class="pt-10">
+      <section class="text-center">
+        <h1 class="text-3xl font-extrabold">Your Journey to Coding Conf 2025 Starts Here!</h1>
+
+        <p class="pt-4 text-xl font-medium text-neutral-300">
+          Secure your spot at next year's biggest coding conference.
+        </p>
+      </section>
+    </main>
   </div>
 </template>
