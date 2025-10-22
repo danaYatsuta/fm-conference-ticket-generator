fac8041bbf7158c3d93c512e6d499f6355512975
Dana Yatsuta <danayatsuta@gmail.com>
2025-10-22 21:50:24 +0300

    Mobile form text inputs and submit button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+import FormInput from './components/FormInput.vue'
+</script>
+
 <template>
-  <div class="background-mobile text-neutral-0 font-inconsolata h-dvh px-4 pt-8">
+  <div class="background-mobile text-neutral-0 font-inconsolata h-dvh px-4 pt-8 text-xl">
     <header class="flex justify-center">
       <img src="./assets/images/logo-full.svg" alt="Coding Conf logo" class="w-40" />
     </header>
@@ -8,10 +12,20 @@
       <section class="text-center">
         <h1 class="text-3xl font-extrabold">Your Journey to Coding Conf 2025 Starts Here!</h1>
 
-        <p class="pt-4 text-xl font-medium text-neutral-300">
+        <p class="pt-4 font-medium text-neutral-300">
           Secure your spot at next year's biggest coding conference.
         </p>
       </section>
+
+      <form class="flex flex-col gap-6 pt-8" @submit.prevent>
+        <FormInput id="name" label="Full Name" />
+        <FormInput id="email" label="Email Address" type="email" placeholder="example@email.com" />
+        <FormInput id="github" label="GitHub Username" placeholder="@yourusername" />
+
+        <button type="submit" class="h-14 rounded-xl bg-orange-500 font-bold text-neutral-900">
+          Generate My Ticket
+        </button>
+      </form>
     </main>
   </div>
 </template>
---
+++ src/components/FormInput.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface Props {
+  id: string
+  label: string
+  type?: string
+  placeholder?: string
+}
+
+const { type = 'text', placeholder = '' } = defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    <label :for="id" class="block text-xl">{{ label }}</label>
+
+    <input
+      :id
+      :type
+      :placeholder
+      class="mt-2 h-14 w-full rounded-xl border border-neutral-500 bg-neutral-700/50 px-4 text-lg backdrop-blur-md placeholder:text-neutral-300"
+    />
+  </div>
+</template>
