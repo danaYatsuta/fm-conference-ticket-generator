41246bdd819bad1cf9b5168e4675da47a366a728
Dana Yatsuta <danayatsuta@gmail.com>
2025-10-31 06:16:15 +0300

    Change some spans with bg image into img elements

--- src/App.vue
+++ src/App.vue
@@ -101,19 +101,17 @@ function onSubmit() {
               class="mt-2 flex h-32 flex-col items-center justify-center gap-4 rounded-xl border border-dashed border-neutral-500 bg-neutral-700/25 outline-offset-2 outline-neutral-500 group-hover:bg-neutral-700/75 peer-focus:outline-2"
             >
               <span
-                class="h-12 w-12 rounded-xl border border-neutral-700 bg-neutral-700/50 bg-[url(images/icon-upload.svg)] bg-center bg-no-repeat"
-                aria-hidden="true"
-              ></span>
+                class="flex h-12 w-12 items-center justify-center rounded-xl border border-neutral-700 bg-neutral-700/50"
+              >
+                <img src="./assets/images/icon-upload.svg" aria-hidden="true" />
+              </span>
 
               <span class="text-lg">Drag and drop or click to upload</span>
             </span>
 
             <span class="flex gap-2 pt-3 text-xs text-neutral-300"
-              ><span
-                class="inline-block h-4 w-4 bg-[url(images/icon-info.svg)]"
-                aria-hidden="true"
-              ></span
-              >Upload your photo (JPG or PNG, max size: 500KB).</span
+              ><img src="./assets/images/icon-info.svg" class="h-4 w-4" aria-hidden="true" />Upload
+              your photo (JPG or PNG, max size: 500KB).</span
             >
           </label>
 
@@ -191,12 +189,9 @@ function onSubmit() {
               <div>
                 <p class="text-lg font-medium">{{ form.name }}</p>
 
-                <p class="flex items-center gap-1 text-xs text-neutral-300">
-                  <span
-                    aria-hidden="true"
-                    class="h-4 w-4 bg-[url(images/icon-github.svg)] bg-contain"
-                  ></span
-                  >{{ form.github }}
+                <p class="flex gap-1 text-xs text-neutral-300">
+                  <img src="./assets/images/icon-github.svg" aria-hidden="true" class="h-4 w-4" />
+                  {{ form.github }}
                 </p>
               </div>
             </div>
